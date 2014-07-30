53d074273d74622b1b66ac26ad9f68104d5f2434
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-07-30 16:59:35 -0300

    Fixing typo in test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var should = require('should')
-  , mongoose = require('mongoose');
+  , mongoose = require('mongoose')
   , mongooseCacher = require('../index.js');
 
 describe('mongoose Query object', function() {
